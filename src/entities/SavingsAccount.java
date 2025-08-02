9705456102e34060e2f7127fb6aa0ba60cc950c6
wilsxnjr <99342358+wilsxnjr@users.noreply.github.com>
2025-08-02 14:07:15 +0200

    Sobreposição

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -25,4 +25,8 @@ public class SavingsAccount extends Account {
         balance += balance * interestRate;
     }
 
+    @Override
+    public void withdraw(Double amount) {
+        balance -= amount;
+    }
 }
